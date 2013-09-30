64dcaa948c548ab66162e49a973535f1c04335d5
yanzhiqiang <f_1s2t3f4f5@126.com>
2013-09-30 22:43:44 +0800

    add huffman tree

---
+++ huffman.cpp
@@ -0,0 +1,12 @@
+#include "huffman.h"
+
+/*
+*	first read 8 bit , calc the frequency,
+*	read source, built map map<frequency,name>
+*	
+*/
+
+int huffman_encode(const char* source,char* dst)
+{
+	
+}
---
+++ huffman.h
@@ -0,0 +1,13 @@
+#ifndef __HUFFMAN_H
+#define __HUFFMAN_H
+
+//need huffman class that have a class to build huffman tree.
+
+
+
+//encode huffman tree
+int huffman_encode(const char* source,char* dst);
+//decode huffman tree
+int huffman_decode(const char* source,char* dst);
+
+#endif
